1d488ad0835441a6499ffc1f36d818e4a912486c
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-21 11:20:51 +0600

    set userContex for firebase config

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB2KIHi3PI8LuUw4f7o1je9vjanrFyC0YA",
+    authDomain: "auth-router-contex-bfddf.firebaseapp.com",
+    projectId: "auth-router-contex-bfddf",
+    storageBucket: "auth-router-contex-bfddf.appspot.com",
+    messagingSenderId: "869137769201",
+    appId: "1:869137769201:web:3d8f002b390658be497f50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
